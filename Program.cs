37112c28bf2a3981af13118720d69d646d529d6c
Jorge <jepers2021@gmail.com>
2021-03-26 22:05:08 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace A133590.Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Como no podemos usar split todavía, se busca el substring "fin".
+            Console.WriteLine("Ejercicio 8");
+            Console.WriteLine("Dada una frase, el programa indica si la palabra \"fin\" se encuentra presente");
+            Console.Write("Por favor, ingrese una frase: ");
+            string frase = Console.ReadLine();
+            if(frase.IndexOf("fin") != -1)
+            {
+                Console.WriteLine("La palabra \"fin\" se encuentra presente en esta frase.");
+            }
+        }
+    }
+}
